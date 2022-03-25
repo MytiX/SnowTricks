c71f0c2178520fa0fa62023fbfcaea1f8945d184
Ferreira <58808696+MytiX@users.noreply.github.com>
2022-03-25 09:33:30 +0100

    Create templating of website (#12)
    * Add assets bootstrap 5
    
    * Update php version (PHP 8)
    
    * Create components elements
    
    * Add asset component of twig
    
    * Create templation of the homepage, detail tricks, edit tricks
    
    * Create controller Home, EditTricks, TricksDetail
    
    * Update img and stylesheet css
    
    * Create folder Security Controller for sign and reset password controller
    
    * Create all templates based on the wireframe
    
    * Changing the config for the annotation on the controller
    
    * Creating the folder of Tricks and add all controllers corresponding
    
    * Add css for sign and forgot password page
    
    * Composer update
    
    * Change the key of annotation.yaml on camelCase
    
    * Change commentary annotation on attribut
    
    * Change the name of the function by __invoke

---
+++ src/Security/Controller/SignInController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Security\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SignInController extends AbstractController
+{
+    #[Route('/signin', name: 'app_signin')]
+    public function __invoke(): Response
+    {
+        return $this->render('sign_in/index.html.twig');
+    }
+}
---
+++ src/Tricks/Controller/EditTricksController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tricks\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EditTricksController extends AbstractController
+{
+    #[Route('/add/tricks', name: 'app_add_tricks')]
+    #[Route('/edit/tricks/{id}', name: 'app_edit_tricks')]
+    public function __invoke(?int $id = null): Response
+    {
+        return $this->render('edit_tricks/index.html.twig');
+    }
+}
