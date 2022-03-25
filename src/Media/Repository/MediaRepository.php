72eb45fbc3e4e7950cf5c2ffd6dc50f2523e2e1f
MyTiX <ferreirajose84700@gmail.com>
2022-03-25 14:50:53 +0100

    Organise architecture of the projet

--- src/Media/Repository/MediaRepository.php
+++ src/Media/Repository/MediaRepository.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Repository;
+namespace App\Media\Repository;
 
-use App\Entity\Media;
+use App\Media\Entity\Media;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
