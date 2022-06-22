9b05488a9ac82ed9144d3d84d9f8f192354a552b
MyTiX <ferreirajose84700@gmail.com>
2022-06-22 15:38:03 +0200

    Add conditions of use for the app

---
+++ src/CGU/Controller/CGUDetailController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\CGU\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CGUDetailController extends AbstractController
+{
+    #[Route('/cgu', 'app_cgu')]
+    public function __invoke()
+    {
+        return $this->render('cgu/index.html.twig');
+    }
+}
